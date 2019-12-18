6dc3340e1555f3662bf74a8aefa0fc040244c45a
san_117 <santiago.cervera117@gmail.com>
2019-12-18 11:27:06 -0300

    [Folder]

---
+++ TrenchVRProject/Assets/Main Menu/Scripts/FolderOption.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class FolderOption : MonoBehaviour
+{
+    private static Action onShow;
+    public Animator anim;
+    public UnityAction onActive;
+
+    private bool selected;
+
+    private void Start()
+    {
+        onShow += Hide;
+    }
+
+    public void Show()
+    {
+        onShow?.Invoke();
+        anim.SetBool("IsOver", true);
+    }
+
+    public void Hide()
+    {
+        anim.SetBool("IsOver", false);
+        selected = false;
+    }
+
+    public void Select()
+    {
+        if (!selected)
+        {
+            onActive.Invoke();
+            selected = true;
+        }
+    }
+}
--- TrenchVRProject/Assets/VR Weaponary/Scripts/Weapons/Folder.cs
+++ TrenchVRProject/Assets/VR Weaponary/Scripts/Weapons/Folder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using OVRTouchSample;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,68 @@ namespace VRWeaponary
 {
     public class Folder : Weapon
     {
-        
+        public List<FolderOption> options = new List<FolderOption>();
+
+        public float overDistance = 0.1f;
+        public float activationDistance = 0.01f;
+
+        private Hand[] hands;
+
+        private Dictionary<Hand, FolderOption> closestOptions = new Dictionary<Hand, FolderOption>();
+
+        protected override void OnInitWeapon()
+        {
+            hands = FindObjectsOfType<Hand>();
+        }
+
+        protected override void UpdateWeapon()
+        {
+            foreach (var pair in closestOptions)
+            {
+                if (Vector3.Distance(pair.Key.transform.position, pair.Value.transform.position) <= activationDistance)
+                {
+                    pair.Value.Select();
+                }
+            }
+
+            foreach (var option in options)
+            {
+                foreach (var hand in hands)
+                {
+                    float distance = Vector3.Distance(hand.transform.position, option.transform.position);
+
+                    if (distance <= overDistance)
+                    {
+                        if (closestOptions.TryGetValue(hand, out FolderOption p))
+                        {
+                            if (Vector3.Distance(hand.transform.position, option.transform.position) > distance)
+                            {
+                                p.Hide();
+                                closestOptions[hand] = option;
+                                option.Show();
+                            }
+                        }
+                        else
+                        {
+                            closestOptions.Add(hand, option);
+                            option.Show();
+                        }
+                    }
+
+                    if (closestOptions.TryGetValue(hand, out var cachePart))
+                    {
+                        if (Vector3.Distance(hand.transform.position, cachePart.transform.position) > overDistance)
+                        {
+                            closestOptions.Remove(hand);
+
+                            if (!closestOptions.ContainsValue(cachePart))
+                            {
+                                cachePart.Hide();
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
